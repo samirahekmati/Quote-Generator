d2a83fdeb9bcad113b260c3b1335628c48b4f7e9
Samira <samirahekmati@gmail.com>
2025-04-29 09:39:54 +0100

    update front-end for form

--- Frontend/script.js
+++ Frontend/script.js
@@ -7,6 +7,42 @@ function fetchAndDisplayQuote() {
     });
 }
 
+
+  
+  //selecting html elements
+const form = document.getElementById("quote-form")
+const quoteInput = document.getElementById("quoteForm");
+const authorInput = document.getElementById("authorForm");
+const validation = document.getElementById("validation")
+
+
+//adding event listener to the form
+form.addEventListener("submit", function(event){
+  event.preventDefault(); // <-- IMPORTANT! Stop the form from reloading the page
+
+  const addedQuote = quoteInput.value
+  console.log("added quote-->", addedQuote)
+
+  const addedAuthor = authorInput.value
+  console.log("added author-->", addedAuthor)
+
+  if(addedAuthor === "" || addedQuote === ""){
+    validation.textContent = "You should fill out the form before submition"
+  validation.style.color = "red"
+  }else {
+    validation.textContent = "Quote successfully saved"
+    validation.style.color = "green"
+    console.log("Quote successfully saved");
+  }
+
+
+  // clear the form after saving
+  form.reset();
+  
+})
+
+
+
 function setup() {
   document.getElementById("new-quote").addEventListener("click", () => {
     fetchAndDisplayQuote(); // Fetch new quote on button click
