1885bac95f08f3458712ab1b8268f2580bf00602
Samira <samirahekmati@gmail.com>
2025-04-29 16:33:05 +0100

    fetch POST frontend

--- Frontend/script.js
+++ Frontend/script.js
@@ -29,15 +29,35 @@ form.addEventListener("submit", function(event){
   if(addedAuthor === "" || addedQuote === ""){
     validation.textContent = "You should fill out the form before submition"
   validation.style.color = "red"
-  }else {
-    validation.textContent = "Quote successfully saved"
-    validation.style.color = "green"
-    console.log("Quote successfully saved");
   }
 
 
-  // clear the form after saving
-  form.reset();
+  fetch("https://cs4cc48o8wwskkwk8oss0884.hosting.codeyourfuture.io/api/quotes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      quote: addedQuote,
+      author: addedAuthor
+    })
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error("Failed to add quote");
+      return res.json();
+    })
+    .then((data) => {
+      validation.textContent = "Quote successfully saved!";
+      validation.style.color = "green";
+      form.reset();
+    })
+    .catch((err) => {
+      validation.textContent = "Error saving quote.";
+      validation.style.color = "red";
+      console.error(err);
+    });
+
+  
   
 })
 
